b9c22424cc23e12ff22404cee59ae8010e6def11
sina rafiei <sina.rafiei@mail.utoronto.ca>
2022-05-17 17:27:46 -0400

    added images

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import Image from './assets/images/ModelX.jpeg'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.titleContainer}>
+        <ImageBackground style={styles.image} source={Image}>
+          <Text style={styles.title}>Model 3</Text>
+          <Text style={styles.subtitle}>Starting at $39,999.</Text>
+        </ImageBackground>
+      </View>
     </View>
   );
 }
@@ -17,4 +22,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    height:"100%",
+    width:"100%"
+  },
+  titleContainer: {
+    height:"100%",
+    width:"100%",
+    alignItems:"center",
+    marginTop:200
+  },
+  title: {
+    fontSize:38
+  },
+  subtitle: {
+    color:"grey"
+  }
 });
